f6e5a9b6263a08c484087227bd872656d72b7c50
retrong <eguobabarth@gmail.com>
2023-08-26 08:46:04 +0100

    implemented create react app

---
+++ src/components/molecules/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../assets/analogue.svg';
+
+const Navbar = () => {
+  return (
+    <nav>
+        <img src={logo} alt="main-logo" className=''/>
+
+    </nav>
+  )
+}
+
+export default Navbar
